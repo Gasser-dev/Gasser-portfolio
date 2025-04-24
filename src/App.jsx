76086bb149aa11bfaaada1532dd358aa3e7d380c
Gasser <gaserzakarya6@gmail.com>
2025-04-25 00:23:54 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import About from './pages/About'
+import About from './pages/About.jsx'
 import Skills from './pages/Skills'
 import gsap from 'gsap'; // <-- import GSAP
 import { useGSAP } from '@gsap/react'; // <-- import the hook from our React package
@@ -13,14 +13,6 @@ import SmallNav from './components/SmallNav';
 import Header from './layout/Header';
 gsap.registerPlugin(useGSAP);
 function App() {
-  const [isLoading, setIsLoading] = useState(true)
-
-  useEffect(() =>{
-    setTimeout(() => {
-      setIsLoading(false)
-    }, 7000);
-  },[])
-  
   return (
     <> {
       isLoading 
